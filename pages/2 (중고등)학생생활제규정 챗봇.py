9b1bca60211786e21ca3c112f489df1489669cbf
starrylife <forinnocen@naver.com>
2024-07-22 16:41:35 +0900

    hide

--- pages/2 (중고등)학생생활제규정 챗봇.py
+++ pages/2 (중고등)학생생활제규정 챗봇.py
@@ -2,6 +2,19 @@ from openai import OpenAI
 import streamlit as st
 import time
 
+hide_github_icon = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK{ display: none; }
+    #MainMenu{ visibility: hidden; }
+    footer { visibility: hidden; }
+    header { visibility: hidden; }
+    </style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 def load_css():
     # 직접 스타일을 삽입하여 배경색을 옅은 하늘색으로 설정
     css = """
